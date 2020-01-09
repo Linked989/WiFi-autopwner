39be2e77db89d3c96275200436550eb28f791f54
Bill Swearingen <hevnsnt@gmail.com>
2020-01-09 16:50:12 -0600

    still just messing with things

--- wifiscan.py
+++ wifiscan.py
@@ -7,11 +7,11 @@ def wifiUp():
 
 
 def internet(host="8.8.8.8", port=53, timeout=3):
-  """
-  Host: 8.8.8.8 (google-public-dns-a.google.com)
-  OpenPort: 53/tcp
-  Service: domain (DNS/TCP)
-  """
+	"""
+	Host: 8.8.8.8 (google-public-dns-a.google.com)
+	OpenPort: 53/tcp
+	Service: domain (DNS/TCP)
+	"""
 	try:
 		socket.setdefaulttimeout(timeout)
 		socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((host, port))
